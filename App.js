7becc36c49989bc62a8b3add8f00434db0ed894e
Anisur Rahman <anisurlist3@gmail.com>
2020-01-20 07:30:50 +0600

    Project Setup

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.header}>This is header</Text>
+        <Text style={styles.body}>This is body</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontWeight: '700'
+  },
+  body: {
+    backgroundColor: 'orange'
+  }
+});
